b6565caecbc9a9e69a580c53673c7f62fb771f8d
unknown <nouirainsaf@gmail.com>
2019-04-12 16:01:38 +0100

    commit web server ye houssein

--- backend/server.js
+++ backend/server.js
@@ -1,66 +1,41 @@
-const express = require("express")
-const { ObjectID, MongoClient } = require("mongodb")
-const multer = require("multer")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const cookieParser = require("cookie-parser")
-const jwt = require("jsonwebtoken")
-const mongoose = require("mongoose")
+const express=require('express')
+const bodyParser=require('body-parser')
+const{ObjectID,MongoClient}=require('mongodb')
+const assert=require('assert')
+const multer= require('multer')
+const mongoose = require('mongoose')
+const cors =require('cors')
 
+const app=express()
 
-const app = express()
 app.use(bodyParser.json())
-const MongoUrl = "mongodb://localhost:27017"
-const database = "badelgiti"
+const MongoUrl='mongodb://localhost:27017'
+const database='badeli'
+
 var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images/uploads")
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname)
-  }
-})
-const upload = multer({
-  storage
-})
+    destination: (req, file, cb) => {
+      cb(null, "uploads")
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + "-" + file.originalname)
+    }
+  })
+  const upload = multer({
+    storage
+  })
+//-------------------uploadimage------------------
 app
   .use(cors())
   .use(express.static("public"))
   .post("/upload", upload.single("image"), (req, res) => {
+    
     if (req.file)
       res.json({
         imageUrl: `images/uploads/${req.file.filename}`
       })
+
     else res.status("409").json("No Files to Upload.")
-})
-MongoClient.connect(MongoUrl, { useNewUrlParser: true }, (err, client) => {
-    err ? console.log("can not connect to database") : (db = client.db(database))
-    
- 
- 
- 
- 
-    app.post("/add-don", (req, res) => {
-      let newactu = req.body
-      db.collection("don").insertOne(newactu, (err, data) => {
-        err ? res.send("cant not add list") : res.send(data)
-      })
   })
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-   })
-
-
- 
-
 
 
 
@@ -186,4 +161,3 @@ app.listen(3055,(err)=>{
     assert.equal(null,err,'server not running')
     console.log('the server is running on port 3055')
     })
-    
