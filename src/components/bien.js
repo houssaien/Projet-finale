a9a9246f43f0dbc6e9f5a1f22437eb995580ef77
houssaien <houssaien.bensalem@esprit.tn>
2019-04-21 21:09:59 +0100

    master 21 avril night

--- src/components/bien.js
+++ src/components/bien.js
@@ -15,24 +15,135 @@ import {
     DropdownToggle,
     DropdownMenu,
     DropdownItem } from 'reactstrap';
-class Don extends Component {
+    import { Card, CardImg, CardText, CardBody,
+      CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+    import {connect} from 'react-redux'
+    import BienItem from './item-bien-front'
+    import Footer from './footer'
+
+import axios from 'axios'
+
+class Bien extends Component {
     constructor(props) {
         super(props);
     
         this.toggle = this.toggle.bind(this);
         this.state = {
-          isOpen: false
+          isOpen: false,
+          activePage: 1
         };
       }
+    
+    
+      componentDidMount=()=>{
+          axios.get('/get-biens').then((res)=>this.props.updateBienReducer(res.data))
+      }
+      
+    
+    
       toggle() {
         this.setState({
           isOpen: !this.state.isOpen
         });
       }
     render() { 
+      const {biens}=this.props
         return ( 
             <div>
-       
+               <header class="header-area">
+     
+        
+               
+
+
+     <Navbar color="white" light expand="md">
+ <NavbarBrand href="/"><img src="front/img/core-img/logof.png" className="logo-navbar" width="200px" height="100px" /></NavbarBrand>
+ <NavbarToggler onClick={this.toggle} />
+ <Collapse isOpen={this.state.isOpen} navbar>
+   <Nav className="ml-auto" > 
+     <NavItem>
+       <NavLink class="classynav nav caret">
+       <Link to='/' className="active">
+       Acceuil
+   </Link>
+   </NavLink>
+     </NavItem>
+
+     <UncontrolledDropdown nav inNavbar>
+       <DropdownToggle nav caret>
+       <a>
+         Echange
+         </a>
+       </DropdownToggle>
+       <DropdownMenu right>
+         <DropdownItem>
+         <Link to='/Bien'>
+         Echange de bien
+   </Link>
+        
+         </DropdownItem>
+         <DropdownItem>
+         <Link to='/Service'>
+           Echange de service
+           </Link>
+         </DropdownItem>
+         
+       </DropdownMenu>
+     </UncontrolledDropdown>
+     <NavItem>
+       <NavLink>
+       <Link to='/Don'>
+       Don
+   </Link>
+          </NavLink>
+     </NavItem>
+
+     <NavItem>
+           <NavLink>
+           <Link to='/Evenement'>
+           Evements
+   </Link></NavLink>
+     </NavItem>
+
+     <NavItem>
+       <NavLink>
+       <Link to='/Apropos'>
+       A propos de nous
+   </Link></NavLink>
+     </NavItem>
+
+     <NavItem>
+       <NavLink>
+       <Link to='/Contact'>
+       Contact
+   </Link>
+          </NavLink>
+     </NavItem>
+     
+{/* Login / Register */}
+
+<NavItem>
+ <a><Link to='/Identifier'>
+ S'identifier
+   </Link> </a>
+   </NavItem>
+   <NavItem>
+   <span><a><Link to='/Registre'>
+   / Registre
+   </Link></a></span>
+   </NavItem>
+
+
+   </Nav>
+   
+ </Collapse>
+</Navbar>
+
+</header>
+
+<section>
+       <div>
        
         <div className="single-welcome-slide">
               {/* Background Curve */}
@@ -47,6 +158,7 @@ class Don extends Component {
                 <div class="col-12">
                     <div class="breadcumb--con">
                         <h2 class="title">Echange de Bien</h2>
+                        <br/>
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a><Link to='/'>
@@ -69,213 +181,84 @@ class Don extends Component {
               <Row>
           <Col sm="4">
           <ListGroup>
-          <ListGroupItem active tag="a"action>Nos Catégories</ListGroupItem>
-          <ListGroupItem tag="a"action>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem tag="a"action>Morbi leo risus</ListGroupItem>
-          <ListGroupItem tag="a"action>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem disabled tag="a"action>Vestibulum at eros</ListGroupItem>
+          <ListGroupItem active tag="a"action><h2>Nos Catégories</h2></ListGroupItem>
+          <ListGroupItem tag="a"action>Immobilier</ListGroupItem>
+          <ListGroupItem tag="a"action>Véhicules</ListGroupItem>
+          <ListGroupItem tag="a"action>Pour la Maison et Jardin</ListGroupItem>
+          <ListGroupItem tag="a"action>Loisirs et Divertissement</ListGroupItem>
+          <ListGroupItem tag="a"action>Informatique et Multimedia</ListGroupItem>
+          <ListGroupItem tag="a"action>Habillement et Bien Etre</ListGroupItem>
+          <ListGroupItem tag="a"action>Autres</ListGroupItem>
         </ListGroup>
+         
                       </Col>
-          <Col sm="8">  Nos biens</Col>
+                          
+          
+          {
+          
+               biens.map((el,index)=>
+               
+              
+            <BienItem key={index} item={el} num={index}/>
+          
+        
+        )}  
+ 
         </Row>
               </Container>
 
+              </div> 
 
-
-
-
-
-
-                </div> 
-
-              <header class="header-area">
-     
-        
-               
-
-
-              <Navbar color="white" light expand="md">
-          <NavbarBrand href="/"><img src="front/img/core-img/logof.png" className="logo-navbar" width="200px" height="100px" /></NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" > 
-              <NavItem>
-                <NavLink class="classynav nav caret">
-                <Link to='/' className="active">
-                Acceuil
-            </Link>
-            </NavLink>
-              </NavItem>
-
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                <a>
-                  Echange
-                  </a>
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                  <Link to='/Bien'>
-                  Echange de bien
-            </Link>
-                 
-                  </DropdownItem>
-                  <DropdownItem>
-                  <Link to='/Service'>
-                    Echange de service
-                    </Link>
-                  </DropdownItem>
-                  
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <NavItem>
-                <NavLink>
-                <Link to='/Don'>
-                Don
-            </Link>
-                   </NavLink>
-              </NavItem>
-
-              <NavItem>
-                    <NavLink>
-                    <Link to='/Evenement'>
-                    Evements
-            </Link></NavLink>
-              </NavItem>
-
-              <NavItem>
-                <NavLink>
-                <Link to='/Apropos'>
-                A propos de nous
-            </Link></NavLink>
-              </NavItem>
-
-              <NavItem>
-                <NavLink>
-                <Link to='/Contact'>
-                Contact
-            </Link>
-                   </NavLink>
-              </NavItem>
-              
-        {/* Login / Register */}
-        
-        <NavItem>
-          <a><Link to='/Identifier'>
-          S'identifier
-            </Link> </a>
-            </NavItem>
-            <NavItem>
-            <span><a><Link to='/Registre'>
-            / Registre
-            </Link></a></span>
-            </NavItem>
-        
-       
-            </Nav>
-            
-          </Collapse>
-        </Navbar>
-        
-        </header>
-        
-        
-              {/* Welcome Content */}
-            
-           
-              </div>
-     
-      
-        {/* ***** Header Area End ***** */}
-
-        
              
-        {/* ***** Welcome Area Start ***** */}
-       
-       
-       
-       
-       
-        {/* ***** Blog Area End ***** */}
+        
+        
+{/* Welcome Content */}
+
+
+</div>
+
+
+{/* ***** Header Area End ***** */}
+
+
+
+{/* ***** Welcome Area Start ***** */}
+
+</div>
+              
+
+
+
+</section>
      
-        <div className="container">
-            <div className="border-line" />
-          </div>
-        {/* ***** Footer Area Start ***** */}
-        <footer className="footer-area section-padding-80-0">
-          <div className="container">
-            <div className="row justify-content-between">
-              {/* Single Footer Widget */}
-              <div className="col-12 col-sm-6 col-lg-3">
-                <div className="single-footer-widget mb-80">
-                  {/* Widget Title */}
-                  <h4 className="widget-title">Contactez Nous</h4>
-                  {/* Footer Content */}
-                  <div className="footer-content mb-15">
-                    <h3>(+216) 73 000 000</h3>
-                    <p>Sousse, Tunisie</p>
-                  </div>
-                  <p className="mb-0">
-                  <img src="front/img/core-img/logof.png" width="200px" height="150px" />
-                    </p>
-                </div>
-              </div>
-              {/* Single Footer Widget */}
-              <div className="col-12 col-sm-6 col-lg-3">
-                <div className="single-footer-widget mb-80">
-                  {/* Widget Title */}
-                  <h4 className="widget-title">badeli</h4>
-                  {/* Nav */}
-                  <nav>
-                    <ul className="our-link">
-                      <li><Link to='/'>Acceuil</Link></li>
-                      <li><Link to='/Bien'>Echange de produit</Link></li>
-                      <li><Link to='/Service'>Echange de service</Link></li>
-                      <li><Link to='/Don'>Don</Link></li>
-                      <li><Link to='/Evenement'>Evénements</Link></li>
-                    </ul>
-                  </nav>
-                </div>
-              </div>
-              {/* Single Footer Widget */}
-              <div className="col-12 col-sm-6 col-lg-3">
-                <div className="single-footer-widget mb-80">
-                  {/* Widget Title */}
-                  <h4 className="widget-title">Mon compte</h4>
-                  {/* Nav */}
-                  <nav>
-                    <ul className="our-link">
-                      <li><Link to='/Registre'>Registre</Link></li>
-                      <li><Link to='/Identifier'>S'identifier</Link></li>
-                      
-                    </ul>
-                  </nav>
-                </div>
-              </div>
-              {/* Single Footer Widget */}
-              <div className="col-12 col-sm-6 col-lg-3">
-                <div className="single-footer-widget mb-80">
-                  {/* Widget Title */}
-                  <h4 className="widget-title">A propos Nous</h4>
-                  <p>le premier site tunisien pour échange de produit et service</p>
-                  {/* Copywrite Text */}
-                  <div className="copywrite-text mb-30">
-                    <p>© Copyright 2019</p>
-                  </div>
-                 
-                </div>
-              </div>
-            </div>
-            <div className="row" style={{marginBottom: '30px'}}>
-              {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
-              Copyright © All rights reserved 2019
-              {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
-            </div>
-          </div>
-        </footer>
+
+      
+  
+    
             </div>
          );
     }
 }
  
-export default Don;
+const mapStateToProps=(state)=>
+{  return {
+    biens:state.reducersbiens
+}
+}
+
+const mapDispatchToProps=(dispatch)=>
+{
+    return {
+        updateBienReducer:biens=>
+        {
+            dispatch({
+                type:'UPDATE_BIEN',
+                biens
+            })
+        }
+        
+    }
+}
+
+ 
+export default connect(mapStateToProps,mapDispatchToProps)(Bien);
